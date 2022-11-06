07e625d7fb494a2aaa70d1f734f11ec32cc3851e
Pratheek Shetty <68729469+gosLp@users.noreply.github.com>
2022-11-06 22:41:42 +0530

    day 1 solved

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -1,4 +1,32 @@
 use std::fs;
 fn main() {
     println!("Hello, world!");
+    let input_file = std::fs::read_to_string("1.txt").unwrap();
+    let input_str = input_file.as_str();
+    let number_increased =  part1(input_str);
+    println!("the number of times the depth increased was: {}", number_increased);
+}
+
+fn part1(input: &str)-> String {
+    input
+    .lines()
+    .fold((0, None), |(mut n, prev), line|{
+        let depth = line.trim().parse::<i64>().unwrap();
+        if let Some(prev) = prev {
+            if depth > prev{
+                n = n+1;
+            }
+            
+        }
+        (n, Some(depth))
+    })
+    .0
+    .to_string()
+}
+
+
+#[test]
+fn test_day1(){
+    assert_eq!("7",
+    part1("199\n200\n208\n210\n200\n207\n240\n269\n260\n263"));
 }
